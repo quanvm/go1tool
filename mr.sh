2b1c382f92fc7bd143f4048e91865a3ff9a36c52
quanvm <quan.vo@go1.com.au>
2018-06-07 11:51:56 +0700

    Update mr.sh

--- mr.sh
+++ mr.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
-id=1
-case "$1" in
-  "exim") id=1199
-  ;;
-  "social") id=1176
-  ;;
-  "note") id=1058
-  ;;
-  "onboard") id=1123
-  ;;
-  "graphin") id=1028
-  ;;
-  "eck") id=1202
-  ;;
-  "portal") id=966
-  ;;
-  "me") id=371
-  ;;
-
-esac
-
 assignee_id=16
-if [ $5 == "duy" ]; then
+if [[ $4 == duy ]]; then
   assignee_id=19
 fi
 
-if [ $id == 1 ]; then
-  echo "Invalid project: $1"
-else
-  http POST https://code.go1.com.au/api/v4/projects/$id/merge_requests private-token:YOUR_TOKEN remove_source_branch=true source_branch=$2 target_branch=$3 title="$4" assignee_id=$assignee_id | jq ".web_url"
+if [[ $2 ]]; then
+  branch=$2
+  service="microservices%2F$1"
+  title=$3
+elif [ -d .git ]; then
+  branch="$(git symbolic-ref -q HEAD)"
+  branch="${branch##refs/heads/}"
+
+  service="$(git config --get remote.origin.url)"
+  service="${service##git@code.go1.com.au:}"
+  service="${service%.git}"
+  service="${service/\//%2F}"
+
+  title=$1
+fi;
+
+remove=true
+target=master
+if [ $branch == master ]; then
+  remove=false
+  target=staging
 fi
+if [ $branch == staging ]; then
+  remove=false
+  target=production
+fi
+
+http POST https://code.go1.com.au/api/v4/projects/$service/merge_requests private-token: remove_source_branch=$remove source_branch=$branch target_branch=$target title="$title" assignee_id=$assignee_id | jq -r ".web_url, .message"
