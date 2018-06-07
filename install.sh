9f2cdfd4d37d2c92242092238d825875a7b6e61f
quanvm <quan.vo@go1.com.au>
2018-06-07 11:50:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-http -h POST https://$1.mygo1.com/v3/$2-service/install
+if [ $1 == local ]; then
+  echo "Local install: $2"
+  http -h POST http://web/GO1/$2/install
+fi
+
+if [ $1 == dev ]; then
+  echo "Dev install: $2"
+  http -h POST https://api-dev.go1.co/$2/install
+fi
+
+if [ $1 == prod ]; then
+  echo "Prod install: $2"
+  http -h POST https://api.go1.co/$2/install
+fi
